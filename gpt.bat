be2ef131cd2ec9ceb52dbfc8eec38d6153b71ef8
Pedro Pacheco <pmpg@hotmail.com>
2024-06-19 23:03:41 -0400

    added more batch scripts for repeat testing and gpt testing

---
+++ gpt.bat
@@ -0,0 +1,8 @@
+@echo off
+IF "%1."=="." GOTO nomodel
+cls
+python analyze_repo.py --repo_path ..\horde-client-bad --model %1 --note %1
+goto end
+:nomodel
+echo You need to specify a model as the first parameter, such as gpt-4
+echo.
---
+++ repeat.bat
@@ -0,0 +1,1 @@
+for /l %%x in (1, 1, 5) do analyze "%1"
